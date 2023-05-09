33eb995efa2b797fa4e4e5ef2cbd448073b1dace
Arisa928 <a.eguchi428@gmail.com>
2023-05-09 02:44:46 +0000

    途中経過

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "home#index"
+  
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
+  
+  resources :users do
+    member do
+      get 'account'
+    end
+    collection do
+      get 'search'
+    end
+    get 'users/:account',controller: 'users', action: 'show'
+  end
+  
+  
+  resources :rooms do
+    collection do
+      get :'search'
+      get :'allroom'
+    end
+  end  
+
+
+  resources :reservations do
+    collection do
+      post :confirm
+    end
+  end  
+  
 end
+
